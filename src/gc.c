6b908aae39308f3aeb3184706086e9d3d7f6ce52
Unknown <namachan10777@gmail.com>
2017-12-22 10:15:04 +0900

    implement gc
    mark&sweepにcompaction足しただけのGC

--- src/gc.c
+++ src/gc.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
 #include "gc.h"
 #include "macro.h"
+#include "env.h"
 //ノードの数
 uint64_t node_num = 0;
 //現在確保されてるノード格納スペースの大きさ
@@ -20,4 +22,54 @@ struct Node* gc_alloc() {
 	struct Node *node = (struct Node*)malloc(sizeof(struct Node));
 	heap[node_num++] = node;
 	return node;
+}
+
+//internal function
+void mark_rec(struct Node* node) {
+	node->visited = true;
+	if (node->tag == Sexp) {
+		mark_rec(node->sexp.car);
+		mark_rec(node->sexp.cdr);
+	}
+}
+
+void mark() {
+	struct Var* var_table = env_vars();
+	size_t var_size = env_var_size();
+	for (size_t i = 0; i < var_size; ++i) {
+		mark_rec(var_table[i].node);
+	}
+}
+
+void sweep() {
+	for (size_t i = 0; i < node_num; ++i) {
+		if (heap[i] == NULL) continue;
+		if (heap[i]->visited) {
+			heap[i]->visited = false;
+		}
+		else {
+			free(heap[i]);
+			heap[i] = NULL;
+		}
+	}
+}
+
+void compaction() {
+	size_t last_idx = 0;
+	for (size_t i = 0; i < node_num; ++i) {
+		if (heap[i] == NULL){
+			--node_num;
+			continue;
+		}
+		else {
+			heap[last_idx] = heap[i];
+			++last_idx;
+		}
+	}
+}
+
+void gc_collect() {
+	mark();
+	sweep();
+	compaction();
 }
