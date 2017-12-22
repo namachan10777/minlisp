ba8115de37aa2850bc9dd06cfae09104530984b1
Unknown <namachan10777@gmail.com>
2017-12-22 18:53:02 +0900

    fix parser
    doted pairに対応

--- src/parser.c
+++ src/parser.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "gc.h"
 #include "node.h"
+#include "macro.h"
 
 char* reserve_str(char* str, size_t *buf_size) {
 	char* buf = malloc(sizeof(char) * *buf_size * 2);
@@ -113,13 +114,21 @@ struct Node* parse(const char *str, size_t *idx) {
 	else if (str[*idx] == '(') {
 		//要素を最後の要素のcdrに代入していく
 		//最初の要素は使わないので静的に確保していい
-		struct Node dummy_root;
-		struct Node* tail = &dummy_root;
+		struct Node** nodes;
+		size_t nodes_reserved_size = 32, nodes_size = 0;
+		INIT(struct Node*, nodes, nodes_reserved_size);
+		bool is_pair = false;
 		for (*idx = *idx+1;;) {
 			//reduce動作
 			if (str[*idx] == ')') {
  				++(*idx);
 				break;
+			}
+			else if (str[*idx] == '.') {
+				if (is_pair) return NULL;
+				++(*idx);
+				is_pair = true;
+				continue;
 			}
 			else if (str[*idx] == EOF) {
 				return NULL;
@@ -129,40 +138,36 @@ struct Node* parse(const char *str, size_t *idx) {
 				continue;
 			}
 			else {
-				struct Node* car = parse(str, idx);
-				if (car == NULL) return NULL;
-				struct Node* pair = gc_alloc();
-				pair->pair.car = car;
-				pair->tag = Pair;
-				tail->pair.cdr = pair;
-				tail = pair;
-				/*
-				(1 2 3)を2までパースした時の状況
-				 (???, cdr) : dummy_root
-				        ↓
-				      (1, cdr) : tail
-				         　　↓
-				           (2, NULL) <- assign
-				*/
+				struct Node* elm = parse(str, idx);
+				if (elm == NULL) return NULL;
+				APPEND(struct Node*, nodes, nodes_reserved_size, nodes_size, elm);
 			}
 		}
-		//リスト末尾にnilを入れる
-		struct Node* nil = gc_alloc();
-		nil->tag = Nil;
-		tail->pair.cdr = nil;
-		/*
-		(1 2 3)をパースし終えた状態
-		 (???, cdr) : dummy_root
-		        ↓
-		     (1, cdr) : dummy_root.cdr
-		      　　↓
-		       (2, cdr)
-		            ↓
-		         (3, cdr) : tail
-		               ↓
-		              nil
-		*/
-		return dummy_root.pair.cdr;
+		if (nodes_size == 1) {
+			return nodes[0];
+		}
+		else if (!is_pair) {
+			struct Node* node = gc_alloc();
+			node->tag = Nil;
+			for (int64_t idx = nodes_size - 1; idx >= 0; --idx) {
+				struct Node* cell = gc_alloc();
+				cell->tag = Pair;
+				cell->pair.car = nodes[idx];
+				cell->pair.cdr = node;
+				node = cell;
+			}
+			return node;
+		}
+		else if (is_pair && nodes_size == 2) {
+			struct Node* pair = gc_alloc();
+			pair->tag = Pair;
+			pair->pair.car = nodes[0];
+			pair->pair.cdr = nodes[1];
+			return pair;
+		}
+		else {
+			return NULL;
+		}
 	}
 	else if (str[*idx] == '"') return parse_str(str, idx);
 	else if (is_digit(str[*idx])) return parse_num(str, idx);
